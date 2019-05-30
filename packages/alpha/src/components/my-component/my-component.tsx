6b9d56fc20f8515ad445d2615964aa10a8c2984d
Rafael Fecha <rfecha@gk-software.com>
2019-05-30 21:21:01 +0200

    feat: add text

--- packages/alpha/src/components/my-component/my-component.tsx
+++ packages/alpha/src/components/my-component/my-component.tsx
@@ -27,6 +27,6 @@ export class MyComponent {
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>Hello, World my friend! I'm {this.getText()}</div>;
   }
 }
